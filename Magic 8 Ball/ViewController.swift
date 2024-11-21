d9a14336b8b0536f97b8199ee14259c4cef0731c
Omar Assidi <oia05@mail.aub.edu>
2024-11-21 23:35:20 +0300

    added constraints

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,10 +11,19 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var askButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
-    private var balls = [#imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball3")]
+    let ball1 = #imageLiteral(resourceName: "ball1")
+    let ball2 = #imageLiteral(resourceName: "ball2")
+    let ball3 = #imageLiteral(resourceName: "ball3")
+    let ball4 = #imageLiteral(resourceName: "ball4")
+    let ball5 = #imageLiteral(resourceName: "ball5")
+    private var balls: [UIImage]?
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        balls = [ball1, ball2, ball3, ball4, ball5]
+    }
     
     @IBAction func onAskButtonPressed(_ sender: UIButton) {
-        imageView.image = balls.randomElement()
+        imageView.image = balls?.randomElement()
         
     }
 }
